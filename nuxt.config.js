d6fab5dd1db750aac5c5df99875224cd3dc9e25b
savvy <savvyversa@gmail.com>
2021-01-29 03:35:24 +0330

    update set port

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,21 +1,24 @@
 import theme from '@nuxt/content-theme-docs'
 
 export default theme({
-  docs: {
-    primaryColor: '#E24F55'
-  },
-  i18n: {
-    locales: () => [{
-      code: 'fa',
-      iso: 'fa-IR',
-      file: 'da-IR.js',
-      name: 'فارسی'
-    }, {
-      code: 'en',
-      iso: 'en-US',
-      file: 'en-US.js',
-      name: 'English'
-    }],
-    defaultLocale: 'en'
-  },
+    server: {
+        port: 8100
+    },
+    docs: {
+        primaryColor: '#E24F55'
+    },
+    i18n: {
+        locales: () => [{
+            code: 'fa',
+            iso: 'fa-IR',
+            file: 'da-IR.js',
+            name: 'فارسی'
+        }, {
+            code: 'en',
+            iso: 'en-US',
+            file: 'en-US.js',
+            name: 'English'
+        }],
+        defaultLocale: 'en'
+    },
 })
