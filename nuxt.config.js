bb521e26bacd6908c804943f6a24822684c0d641
savvy <savvyversa@gmail.com>
2021-02-03 11:48:30 +0330

    update nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,12 @@
 import theme from '@nuxt/content-theme-docs'
 
 export default theme({
+    buildModules: [
+        '@nuxtjs/google-analytics'
+    ],
+    googleAnalytics: {
+        id: 'UA-104158634-1'
+    },
     server: {
         port: 8150
     },
